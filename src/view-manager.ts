2c713260f12b05f8dd5e56d668ab2ccd040b3f72
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-26 17:39:56 +0100

    [ABP-32] Added a bark for request to phrasal verbs from current note. Thanks https://github.com/HyeonseoNam/auto-classifier

---
+++ src/view-manager.ts
@@ -0,0 +1,117 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { App, MarkdownView, Editor, FrontMatterCache } from "obsidian";
+
+export class ViewManager {
+	constructor(app: App) {
+		this.app = app;
+	}
+
+	app: App;
+
+	async getSelection(editor?: Editor): Promise<string | null> {
+		if (editor) {
+			return editor.getSelection();
+		}
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+			return activeView.editor.getSelection();
+		}
+		return null;
+	}
+
+	async getTitle(): Promise<string | null> {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+			return activeView.file!.basename;
+		}
+		return null;
+	}
+
+	async getContent(): Promise<string | null> {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+			// delete frontmatter
+			let content = activeView.getViewData();
+			const file = activeView.file!;
+			const frontmatter: FrontMatterCache | undefined =
+				this.app.metadataCache.getFileCache(file)?.frontmatter;
+			if (frontmatter) {
+				content = content.split("---").slice(2).join("---");
+			}
+			return content;
+		}
+		return null;
+	}
+
+	async getTags(filterRegex?: string): Promise<string[] | null> {
+		//@ts-ignore
+		const tagsDict = this.app.metadataCache.getTags();
+		let tags = Object.keys(tagsDict);
+		if (!tags || tags.length == 0) return null;
+		// remove #
+		tags = tags.map((tag) => tag.replace(/^#/, ""));
+		// filter
+		if (filterRegex) {
+			return tags.filter((tag) => RegExp(filterRegex).test(tag));
+		}
+		return tags;
+	}
+
+	async insertAtTitle(value: string, overwrite = false): Promise<void> {
+		const file = this.app.workspace.getActiveFile();
+		if (!file) return;
+		let newName = file.basename;
+		if (overwrite) {
+			newName = `${value}`;
+		} else {
+			newName = `${newName} ${value}`;
+		}
+		// for windows file name
+		newName = newName.replace(/["/<>:|?"]/g, "");
+		// @ts-ignore
+		const newPath = file.getNewPathAfterRename(newName);
+		await this.app.fileManager.renameFile(file, newPath);
+	}
+
+	async insertAtCursor(value: string, overwrite = false): Promise<void> {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const output = this.preprocessOutput(value);
+
+		if (activeView) {
+			const editor = activeView.editor;
+			const selection = editor.getSelection();
+			if (selection && !overwrite) {
+				// replace selection
+				editor.setSelection(editor.getCursor("to"));
+			}
+			// overwrite
+			editor.replaceSelection(output);
+		}
+	}
+
+	async insertAtContentTop(value: string): Promise<void> {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const output = this.preprocessOutput(value);
+
+		if (activeView) {
+			const editor = activeView.editor;
+			const file = activeView.file!;
+			const sections =
+				this.app.metadataCache.getFileCache(file)?.sections;
+
+			// get the line after frontmatter
+			let topLine = 0;
+			if (sections && sections[0].type == "yaml") {
+				topLine = sections[0].position.end.line + 1;
+			}
+
+			// replace top of the content
+			editor.setCursor({ line: topLine, ch: 0 });
+			editor.replaceSelection(`${output}\n`);
+		}
+	}
+
+	preprocessOutput(value: string): string {
+		return value;
+	}
+}
