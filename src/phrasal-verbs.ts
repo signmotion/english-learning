8de335e279ec193f9f05b510aabe61fa5384e24e
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-28 00:34:21 +0100

    [ABP-32] Remove shorter keys that start with the same keys.

--- src/phrasal-verbs.ts
+++ src/phrasal-verbs.ts
@@ -22,13 +22,12 @@ export class PhrasalVerbs extends Task {
 		await this.constructNewNote(newContent);
 	}
 
-	async contentFromSourceNote(
-		content: string
-	): Promise<Record<string, string>> {
+	async contentFromSourceNote(content: string): Promise<Map<string, string>> {
 		console.log("Getting content from source note...");
 
 		console.log("Requesting phrasal verbs by content...");
 		const answer = await this.request(content);
+		console.log(answer);
 		const siftedAnswer = this.siftAnswerBySource(content, answer);
 		console.log(siftedAnswer);
 
@@ -37,26 +36,38 @@ export class PhrasalVerbs extends Task {
 
 	siftAnswerBySource(
 		content: string,
-		answer: Record<string, string>
-	): Record<string, string> {
+		answer: Map<string, unknown>
+	): Map<string, string> {
 		// TODO Be able to look for inaccurate matches.
 		console.log("Removing inexact matches...");
-		const r: Record<string, string> = {};
 
+		// left keys 100% mathed with source
+		const r: Map<string, string> = new Map<string, string>();
 		const lowercaseContent = content.toLowerCase();
 		for (const [first, second] of Object.entries(answer)) {
 			// [first] always in lowercase
 			if (lowercaseContent.indexOf(first) != -1) {
-				r[first] = second;
+				r.set(first, second as string);
 			}
 		}
 
-		return r;
+		// remove shorter keys that start with the same keys
+		const filteredEntries = [...r].filter(([k, v]) => {
+			for (const [s] of r) {
+				if (s.length != k.length && s.startsWith(k)) {
+					return false;
+				}
+			}
+			return true;
+		});
+		const filteredMap = new Map<string, string>(filteredEntries);
+
+		return filteredMap;
 	}
 
-	async constructNotesByAnswer(answer: Record<string, string>) {
+	async constructNotesByAnswer(answer: Map<string, string>) {
 		console.log("Construct notes with phrasal verbs consider to answer...");
-		for (const [first, second] of Object.entries(answer)) {
+		for (const [first, second] of answer.entries()) {
 			//console.log(`${first} -> ${second}`);
 			await this.viewManager.createNoteMd(first, second);
 		}
@@ -64,12 +75,19 @@ export class PhrasalVerbs extends Task {
 
 	async addLinksToSource(
 		content: string,
-		answer: Record<string, string>
+		answer: Map<string, string>
 	): Promise<string> {
 		console.log("Linking the source to the created notes...");
+
+		// we need sort answer because sometimes able something
+		// like `work out` and `work out well` and we lost link
+		const sortedAnswer = new Map([...answer.entries()].sort());
+		console.log(sortedAnswer);
+
 		let newContent = content;
 		let lowercaseContent = newContent.toLowerCase();
-		for (const first of Object.keys(answer)) {
+		for (const first of sortedAnswer.keys()) {
+			console.log(first);
 			// [first] always in lowercase
 			for (let q = 0; ; ) {
 				const i = lowercaseContent.indexOf(first, q);
@@ -77,9 +95,9 @@ export class PhrasalVerbs extends Task {
 					break;
 				}
 
-				if (i > 0 && lowercaseContent[i - 1] != "[") {
+				if (i > 0 && lowercaseContent.at(i - 1) != "[") {
 					// linking [first]
-					const before = newContent.substring(i, -12);
+					const before = newContent.substring(0, i);
 					const after = newContent.substring(i + first.length);
 					newContent = `${before}[[${first}]]${after}`;
 					lowercaseContent = newContent.toLowerCase();
@@ -98,7 +116,7 @@ export class PhrasalVerbs extends Task {
 		await this.viewManager.createNoteMd(title, newContent, true);
 	}
 
-	async request(text: string): Promise<Record<string, string>> {
+	async request(text: string): Promise<Map<string, unknown>> {
 		const baseUrl = "http://127.0.0.1:8000/";
 		const headers = {
 			"Content-Type": "application/json",
